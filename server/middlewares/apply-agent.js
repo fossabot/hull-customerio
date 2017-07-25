c2c70b0b560616f2404c7e728100ca6e2c2a671f
mkawalek <michalkawalekdevkit@icloud.com>
2017-07-25 15:06:46 +0200

    bottleneck moved to customerioClient

--- server/middlewares/apply-agent.js
+++ server/middlewares/apply-agent.js
@@ -1,12 +1,20 @@
 /* @flow */
 import { Request, Response, Next } from "express";
-import SyncAgent from "../lib/sync-agent";
 import Bottleneck from "bottleneck";
+import _ from "lodash";
+import SyncAgent from "../lib/sync-agent";
+import CustomerioClient from "../lib/customerio-client";
 
 export default function applyAgent(bottleneck: Bottleneck) {
   return (req: Request, res: Response, next: Next) => {
     req.hull.service = req.hull.service || {};
-    req.hull.service.syncAgent = new SyncAgent(req.hull, bottleneck);
+
+    const customerioClient = new CustomerioClient(
+      _.get(req.hull.ship, "private_settings.site_id"),
+      _.get(req.hull.ship, "private_settings.api_key"),
+      bottleneck);
+
+    req.hull.service.syncAgent = new SyncAgent(req.hull, customerioClient);
     return next();
   };
 }
