6e84d666d5c9e9f9729c6fffb4c517a01db83e65
mkawalek <michalkawalekdevkit@icloud.com>
2017-07-31 12:49:42 +0200

    minor fixes, mostly cleaning

--- test/integration/support/bootstrap.js
+++ test/integration/support/bootstrap.js
@@ -9,6 +9,5 @@ export default function bootstrap() {
   const connector = new Connector({ hostSecret: "1234", port: 8000, clientConfig: { protocol: "http", firehoseUrl: "firehose" } });
   connector.setupApp(app);
   server(app, new Cluster(30, 34));
-  connector.startWorker();
   return connector.startApp(app);
 }
