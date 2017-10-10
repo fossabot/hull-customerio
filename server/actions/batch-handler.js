5814eac960f0d2b7af38320edf7ede6b13072f9d
Sven Maschek <sven.maschek@outlook.de>
2017-10-10 15:36:20 +0200

    Fixed usersToSend within batchHandler

--- server/actions/batch-handler.js
+++ server/actions/batch-handler.js
@@ -2,12 +2,12 @@
 import _ from "lodash";
 
 export default function batchHandler({ ship, service: { syncAgent } }: Object, messages: Array<Object> = []) {
-  const usersToSend = [];
   const filterSegments = _.get(ship, "private_settings.synchronized_segments");
 
   // Do always check whitelisted segments, but only check users to delete
   // if flag is set:
   if (_.get(ship, "private_settings.enable_user_deletion")) {
+    const usersToSend = [];
     const usersToDelete = [];
 
     messages.forEach(message => {
@@ -15,11 +15,12 @@ export default function batchHandler({ ship, service: { syncAgent } }: Object, m
       else usersToDelete.push(message.user);
     });
 
+    messages.forEach(message => {
+      if (_.intersection(message.segments.map(s => s.id), filterSegments).length > 0) usersToSend.push({ user: message.user, segments: message.segments });
+    });
+
     return syncAgent.deleteBatchOfUsers(usersToDelete).then(() => syncAgent.sendBatchOfUsers(usersToSend));
   }
 
-  messages.forEach(message => {
-    if (_.intersection(message.segments.map(s => s.id), filterSegments).length > 0) usersToSend.push({ user: message.user, segments: message.segments });
-  });
   return syncAgent.sendBatchOfUsers(messages.map(m => ({ user: m.user, segments: m.segments })));
 }
